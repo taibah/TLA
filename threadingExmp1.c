8b418f1aa1b8996db94e80c68d9c23d0dbdb7e7b
Muhammad Taibah <taibah@computer.org>
2014-08-27 20:58:25 -0400

    Update threadingExmp1.c

--- threadingExmp1.c
+++ threadingExmp1.c
@@ -1,36 +1,39 @@
-#include <stdio.h>
 #include <assert.h>
 #include <pthread.h>
+#include <cstdlib>
+#include <iostream>
+using namespace std;
 
 void *mythread(void *arg) {
-
-    int x = (int *)arg;
-
-    printf("%d\n", x);
+    int num;
+    num = (intptr_t)arg;
+    int i =0;
+    for(i=0;i<10000;i++)
+       cout << num << endl;
     return NULL;
 
 }
 
-int main(int argc, char *argv[]) 
+int main(int argc, char *argv[])
 {
 
-    pthread_t p[10];
+    pthread_t p[100];
     int i;
     int rc;
 
-    printf("main: begin\n");
+    cout << "main: begin\n";
 
-    for(i=0; i<10; i++){
-        rc = pthread_create(&p[i], NULL, mythread,(void *) i); 
+    for(i=0; i<100; i++){
+        rc = pthread_create(&p[i], NULL, mythread, (void *)i);
         assert(rc == 0);
     }
 
-    for(i=0; i<10; i++){
+    for(i=0; i<100; i++){
         rc = pthread_join(p[i], NULL);
         assert(rc == 0);
     }
 
-    printf("main: end\n");
+    cout << "main: end\n";
 
     return 0;
 
